2d04a6ee0275dc927e3454321e3c2737a6ff1d47
Flu Sileachim <mail@sileachim.de>
2026-01-07 12:16:09 +0100

    install devtools-json

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,20 +1,23 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import fs from 'fs';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: { include: ['src/**/*.{test,spec}.{js,ts}'] },
 	ssr: { noExternal: ['three'] },
-	...(process.env.WANDERER_ENV == "dev" ? {
-		server: {
-			// https: {
+
+	...process.env.WANDERER_ENV == "dev"
+		? {
+			server: {// https: {
 			// 	key: fs.readFileSync('.svelte-kit/key.pem'),
 			// 	cert: fs.readFileSync('.svelte-kit/cert.pem')
 			// },
 			// host: true, // true
 			// port: 443 // 443
+			}
 		}
-	} : {})
+		: {}
 });
