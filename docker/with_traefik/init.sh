1f76b2d956909fc4e3becc484daee31e81a8dedd
Ulf Michaelis <ulf@sileachim.de>
2026-02-10 09:45:52 +0100

    cp traefik.yaml into traefik workdir

--- docker/with_traefik/init.sh
+++ docker/with_traefik/init.sh
@@ -5,6 +5,7 @@ source .env
 mkdir -p ${TRAEFIK_BASE}/tmp
 touch ${TRAEFIK_BASE}/acme.json
 chmod u=rw,go= ${TRAEFIK_BASE}/acme.json
+cp traefik.yaml ${TRAEFIK_BASE}/
 
 mkdir -p ${WANDERER_BASE}/data.ms
 mkdir -p ${WANDERER_BASE}/pb_data
