c8bcf3fa4ac2e4bbf3efbfcd3146f55336c0a337
Ulf Michaelis <ulf@sileachim.de>
2026-02-10 09:43:49 +0100

    mkdir with -p is better.

--- docker/with_traefik/init.sh
+++ docker/with_traefik/init.sh
@@ -2,11 +2,11 @@
 
 source .env
 
-mkdir ${TRAEFIK_BASE}/tmp
+mkdir -p ${TRAEFIK_BASE}/tmp
 touch ${TRAEFIK_BASE}/acme.json
 chmod u=rw,go= ${TRAEFIK_BASE}/acme.json
 
-mkdir ${WANDERER_BASE}/data.ms
-mkdir ${WANDERER_BASE}/pb_data
-mkdir ${WANDERER_BASE}/uploads
+mkdir -p ${WANDERER_BASE}/data.ms
+mkdir -p ${WANDERER_BASE}/pb_data
+mkdir -p ${WANDERER_BASE}/uploads
 
